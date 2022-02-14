8e3b90eb200b5c0115ee04446d5d13518bb2105e
Juan Ignacio Amarillo <amarillojuanignacio8@gmail.com>
2022-02-14 20:58:40 -0300

    REPARACION

---
+++ CONTEXTO/Migrations/202202142356503_ACTUIALIZACION4.cs
@@ -0,0 +1,18 @@
+﻿namespace CONTEXTO.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ACTUIALIZACION4 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.USUARIOs", "FECHA", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.USUARIOs", "FECHA");
+        }
+    }
+}
