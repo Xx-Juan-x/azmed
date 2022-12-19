ea9befebee0531f7fc8241d9a4d6b0a40730d5ce
JuanIgnacioAmarilloJacob <amarillojuanignacio8@gmail.com>
2022-12-19 15:30:45 -0300

    IMPLEMENTACIÓN PATRON FACADE

--- VISTA/frmLISTA_TURNOS_PACIENTES.cs
+++ VISTA/frmLISTA_TURNOS_PACIENTES.cs
@@ -12,6 +12,59 @@ namespace VISTA
 {
     public partial class frmLISTA_TURNOS_PACIENTES : Form
     {
+        public class Facade
+        {
+            ARMAR_GRILLA_A one;
+            ARMAR_GRILLA_B two;
+            public Facade()
+            {
+                one = new ARMAR_GRILLA_A { };
+                two = new ARMAR_GRILLA_B { };
+            }
+            public dynamic METODO_A()
+            {
+                return one.MOSTRAR();
+            }
+            public dynamic METODO_B()
+            {
+                return two.MOSTRAR();
+            }
+        }
+        public class ARMAR_GRILLA_A
+        {
+            protected CONTROLADORA.TURNOS cTURNOS = CONTROLADORA.TURNOS.OBTENER_INSTANCIA();
+            protected CONTROLADORA.USUARIOS cUSUARIOS = CONTROLADORA.USUARIOS.OBTENER_INSTANCIA();
+            protected DateTime DIA_ACTUAL = DateTime.Now;
+            protected int HORA_ACTUAL_INT = Convert.ToInt32(Convert.ToString(DateTime.Now.Hour));
+            public dynamic MOSTRAR()
+            {
+                var LISTA_TURNOS_PACIENTE = (from a in cTURNOS.OBTENER_TURNOS()
+                                             where a.PACIENTE.ID_USUARIO == frmLOGIN.ID_USUARIO
+                                             && (a.FECHA < DIA_ACTUAL)
+                                             || (a.FECHA == DIA_ACTUAL && a.HORA_TURNO < HORA_ACTUAL_INT)
+                                             select a).ToList();
+                return LISTA_TURNOS_PACIENTE;
+
+            }
+        }
+        public class ARMAR_GRILLA_B
+        {
+            protected CONTROLADORA.TURNOS cTURNOS = CONTROLADORA.TURNOS.OBTENER_INSTANCIA();
+            protected CONTROLADORA.USUARIOS cUSUARIOS = CONTROLADORA.USUARIOS.OBTENER_INSTANCIA();
+            protected DateTime DIA_ACTUAL = DateTime.Now;
+            protected int HORA_ACTUAL_INT = Convert.ToInt32(Convert.ToString(DateTime.Now.Hour));
+            public dynamic MOSTRAR()
+            {
+                var LISTA_TURNOS_PACIENTE = (from a in cTURNOS.OBTENER_TURNOS()
+                                             where a.PACIENTE.ID_USUARIO == frmLOGIN.ID_USUARIO
+                                             && (a.FECHA > DIA_ACTUAL)
+                                             || (a.FECHA == DIA_ACTUAL && a.HORA_TURNO > HORA_ACTUAL_INT)
+                                             select a).ToList();
+                return LISTA_TURNOS_PACIENTE;
+
+            }
+        }
+
         private static frmLISTA_TURNOS_PACIENTES instancia;
 
         public static frmLISTA_TURNOS_PACIENTES OBTENER_INSTANCIA()
@@ -26,54 +79,32 @@ namespace VISTA
             }
             return instancia;
         }
-
-        private CONTROLADORA.TURNOS cTURNOS;
-        private CONTROLADORA.USUARIOS cUSUARIOS;       
-
-
-        public frmLISTA_TURNOS_PACIENTES()
+        private frmLISTA_TURNOS_PACIENTES()
         {
             InitializeComponent();
-            cTURNOS = CONTROLADORA.TURNOS.OBTENER_INSTANCIA();
-            cUSUARIOS = CONTROLADORA.USUARIOS.OBTENER_INSTANCIA();
             rbLISTA_TURNOS_PROXIMOS.Checked = true;
             ARMA_GRILLA("B");
         }
-
+        
         public void ARMA_GRILLA(string TIPO)
         {
-            DateTime DIA_ACTUAL = DateTime.Now;
-            //13:44:00
-            string HORA_ACTUAL = Convert.ToString(DIA_ACTUAL.Hour);
-            int HORA_ACTUAL_INT = Convert.ToInt32(HORA_ACTUAL);
-
             if (TIPO == "A")
             {
                 if (rbLISTA_TURNOS_PASADO.Checked)
                 {
-                    var LISTA_TURNOS_PACIENTE = (from a in cTURNOS.OBTENER_TURNOS()
-                                                 where a.PACIENTE.ID_USUARIO == frmLOGIN.ID_USUARIO
-                                                 && (a.FECHA < DIA_ACTUAL)
-                                                 || (a.FECHA == DIA_ACTUAL && a.HORA_TURNO < HORA_ACTUAL_INT)
-                                                 select a).ToList();
-
+                    Facade facade = new Facade();
                     dgvLISTA_TURNOS.DataSource = null;
-                    dgvLISTA_TURNOS.DataSource = LISTA_TURNOS_PACIENTE;
+                    dgvLISTA_TURNOS.DataSource = facade.METODO_A();
+
                 }
             }
             else if (TIPO == "B")
             {
                 if (rbLISTA_TURNOS_PROXIMOS.Checked)
                 {
-
-                    var LISTA_TURNOS_PACIENTE = (from a in cTURNOS.OBTENER_TURNOS()
-                                                 where a.PACIENTE.ID_USUARIO == frmLOGIN.ID_USUARIO
-                                                 && (a.FECHA > DIA_ACTUAL)
-                                                 || (a.FECHA == DIA_ACTUAL && a.HORA_TURNO > HORA_ACTUAL_INT)
-                                                 select a).ToList();
-
+                    Facade facade = new Facade();
                     dgvLISTA_TURNOS.DataSource = null;
-                    dgvLISTA_TURNOS.DataSource = LISTA_TURNOS_PACIENTE;
+                    dgvLISTA_TURNOS.DataSource = facade.METODO_B();
                 }
             }
         }
