f7bf7089433080f090dffd63198053459feb3d9a
JuanIgnacioAmarilloJacob <amarillojuanignacio8@gmail.com>
2023-02-08 21:38:03 -0300

    ARREGLO CONTEXTO

---
+++ CONTEXTO/Migrations/202302090037279_CAMBIOS_ACTUALES.cs
@@ -0,0 +1,16 @@
+﻿namespace CONTEXTO.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CAMBIOS_ACTUALES : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
