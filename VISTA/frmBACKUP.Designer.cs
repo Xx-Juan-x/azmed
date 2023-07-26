96267a6b9311ce5c878cced3e92d858ae78c7d32
JuanIgnacioAmarilloJacob <amarillojuanignacio8@gmail.com>
2023-07-25 22:32:49 -0300

    RESGUARDO Y RESTAURACION DE DATOS FINALIZADO

--- VISTA/frmBACKUP.Designer.cs
+++ VISTA/frmBACKUP.Designer.cs
@@ -29,22 +29,191 @@ namespace VISTA
         /// </summary>
         private void InitializeComponent()
         {
+            this.gbBACKUP_DATABASE = new System.Windows.Forms.GroupBox();
+            this.lbl_LOCALIZACION = new System.Windows.Forms.Label();
+            this.txtLOCALIZACION1 = new System.Windows.Forms.TextBox();
+            this.btnBACKUP = new System.Windows.Forms.Button();
+            this.btnNAVEGADOR1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRESTAURAR = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNAVEGADOR2 = new System.Windows.Forms.Button();
+            this.txtLOCALIZACION2 = new System.Windows.Forms.TextBox();
+            this.btnCERRAR = new System.Windows.Forms.Button();
+            this.gbBACKUP_DATABASE.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // gbBACKUP_DATABASE
+            // 
+            this.gbBACKUP_DATABASE.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbBACKUP_DATABASE.Controls.Add(this.btnBACKUP);
+            this.gbBACKUP_DATABASE.Controls.Add(this.lbl_LOCALIZACION);
+            this.gbBACKUP_DATABASE.Controls.Add(this.btnNAVEGADOR1);
+            this.gbBACKUP_DATABASE.Controls.Add(this.txtLOCALIZACION1);
+            this.gbBACKUP_DATABASE.ForeColor = System.Drawing.Color.White;
+            this.gbBACKUP_DATABASE.Location = new System.Drawing.Point(11, 12);
+            this.gbBACKUP_DATABASE.Name = "gbBACKUP_DATABASE";
+            this.gbBACKUP_DATABASE.Size = new System.Drawing.Size(378, 93);
+            this.gbBACKUP_DATABASE.TabIndex = 8;
+            this.gbBACKUP_DATABASE.TabStop = false;
+            this.gbBACKUP_DATABASE.Text = "BACKUP DE LA BASE DE DATOS";
+            // 
+            // lbl_LOCALIZACION
+            // 
+            this.lbl_LOCALIZACION.AutoSize = true;
+            this.lbl_LOCALIZACION.Location = new System.Drawing.Point(6, 43);
+            this.lbl_LOCALIZACION.Name = "lbl_LOCALIZACION";
+            this.lbl_LOCALIZACION.Size = new System.Drawing.Size(87, 13);
+            this.lbl_LOCALIZACION.TabIndex = 10;
+            this.lbl_LOCALIZACION.Text = "LOCALIZACION:";
+            // 
+            // txtLOCALIZACION1
+            // 
+            this.txtLOCALIZACION1.Location = new System.Drawing.Point(99, 40);
+            this.txtLOCALIZACION1.Name = "txtLOCALIZACION1";
+            this.txtLOCALIZACION1.Size = new System.Drawing.Size(172, 20);
+            this.txtLOCALIZACION1.TabIndex = 13;
+            // 
+            // btnBACKUP
+            // 
+            this.btnBACKUP.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.btnBACKUP.FlatAppearance.BorderSize = 0;
+            this.btnBACKUP.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBACKUP.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBACKUP.ForeColor = System.Drawing.Color.White;
+            this.btnBACKUP.Location = new System.Drawing.Point(277, 54);
+            this.btnBACKUP.Name = "btnBACKUP";
+            this.btnBACKUP.Size = new System.Drawing.Size(90, 28);
+            this.btnBACKUP.TabIndex = 92;
+            this.btnBACKUP.Text = "Backup";
+            this.btnBACKUP.UseVisualStyleBackColor = false;
+            this.btnBACKUP.Click += new System.EventHandler(this.btnBACKUP_Click);
+            // 
+            // btnNAVEGADOR1
+            // 
+            this.btnNAVEGADOR1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.btnNAVEGADOR1.FlatAppearance.BorderSize = 0;
+            this.btnNAVEGADOR1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNAVEGADOR1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNAVEGADOR1.ForeColor = System.Drawing.Color.White;
+            this.btnNAVEGADOR1.Location = new System.Drawing.Point(277, 19);
+            this.btnNAVEGADOR1.Name = "btnNAVEGADOR1";
+            this.btnNAVEGADOR1.Size = new System.Drawing.Size(90, 29);
+            this.btnNAVEGADOR1.TabIndex = 91;
+            this.btnNAVEGADOR1.Text = "Navegador";
+            this.btnNAVEGADOR1.UseVisualStyleBackColor = false;
+            this.btnNAVEGADOR1.Click += new System.EventHandler(this.btnNAVEGADOR1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnRESTAURAR);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnNAVEGADOR2);
+            this.groupBox1.Controls.Add(this.txtLOCALIZACION2);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(11, 120);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(378, 97);
+            this.groupBox1.TabIndex = 93;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "BACKUP DE LA BASE DE DATOS";
+            // 
+            // btnRESTAURAR
+            // 
+            this.btnRESTAURAR.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.btnRESTAURAR.FlatAppearance.BorderSize = 0;
+            this.btnRESTAURAR.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRESTAURAR.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRESTAURAR.ForeColor = System.Drawing.Color.White;
+            this.btnRESTAURAR.Location = new System.Drawing.Point(277, 54);
+            this.btnRESTAURAR.Name = "btnRESTAURAR";
+            this.btnRESTAURAR.Size = new System.Drawing.Size(90, 28);
+            this.btnRESTAURAR.TabIndex = 92;
+            this.btnRESTAURAR.Text = "Restaurar";
+            this.btnRESTAURAR.UseVisualStyleBackColor = false;
+            this.btnRESTAURAR.Click += new System.EventHandler(this.btnRESTAURAR_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "LOCALIZACION:";
+            // 
+            // btnNAVEGADOR2
+            // 
+            this.btnNAVEGADOR2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.btnNAVEGADOR2.FlatAppearance.BorderSize = 0;
+            this.btnNAVEGADOR2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNAVEGADOR2.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNAVEGADOR2.ForeColor = System.Drawing.Color.White;
+            this.btnNAVEGADOR2.Location = new System.Drawing.Point(277, 19);
+            this.btnNAVEGADOR2.Name = "btnNAVEGADOR2";
+            this.btnNAVEGADOR2.Size = new System.Drawing.Size(90, 29);
+            this.btnNAVEGADOR2.TabIndex = 91;
+            this.btnNAVEGADOR2.Text = "Navegador";
+            this.btnNAVEGADOR2.UseVisualStyleBackColor = false;
+            this.btnNAVEGADOR2.Click += new System.EventHandler(this.btnNAVEGADOR2_Click);
+            // 
+            // txtLOCALIZACION2
+            // 
+            this.txtLOCALIZACION2.Location = new System.Drawing.Point(99, 40);
+            this.txtLOCALIZACION2.Name = "txtLOCALIZACION2";
+            this.txtLOCALIZACION2.Size = new System.Drawing.Size(172, 20);
+            this.txtLOCALIZACION2.TabIndex = 13;
+            // 
+            // btnCERRAR
+            // 
+            this.btnCERRAR.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.btnCERRAR.FlatAppearance.BorderSize = 0;
+            this.btnCERRAR.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCERRAR.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCERRAR.ForeColor = System.Drawing.Color.White;
+            this.btnCERRAR.Location = new System.Drawing.Point(141, 225);
+            this.btnCERRAR.Name = "btnCERRAR";
+            this.btnCERRAR.Size = new System.Drawing.Size(101, 34);
+            this.btnCERRAR.TabIndex = 93;
+            this.btnCERRAR.Text = "Cerrar";
+            this.btnCERRAR.UseVisualStyleBackColor = false;
+            this.btnCERRAR.Click += new System.EventHandler(this.btnCERRAR_Click);
             // 
             // frmBACKUP
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
-            this.ClientSize = new System.Drawing.Size(497, 322);
+            this.ClientSize = new System.Drawing.Size(401, 271);
+            this.Controls.Add(this.btnCERRAR);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbBACKUP_DATABASE);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "frmBACKUP";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "BACKUP DEL SISTEMA";
+            this.gbBACKUP_DATABASE.ResumeLayout(false);
+            this.gbBACKUP_DATABASE.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbBACKUP_DATABASE;
+        private System.Windows.Forms.Label lbl_LOCALIZACION;
+        private System.Windows.Forms.TextBox txtLOCALIZACION1;
+        private System.Windows.Forms.Button btnBACKUP;
+        private System.Windows.Forms.Button btnNAVEGADOR1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnRESTAURAR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNAVEGADOR2;
+        private System.Windows.Forms.TextBox txtLOCALIZACION2;
+        private System.Windows.Forms.Button btnCERRAR;
     }
 }
--- VISTA/frmBACKUP.cs
+++ VISTA/frmBACKUP.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace VISTA
 {
@@ -27,12 +28,85 @@ namespace VISTA
             return instancia;
         }
 
+        SqlConnection CONEXION = new SqlConnection("Data Source=JUAN\\SQLEXPRESS;Initial Catalog=CONTEXTO.CLINICA_AZMED;Integrated Security=True");
+
         public frmBACKUP()
         {
             InitializeComponent();
         }
 
+        private void btnNAVEGADOR1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtLOCALIZACION1.Text = dlg.SelectedPath;
+                btnBACKUP.Enabled = true;
+            }
+        }
 
+        private void btnBACKUP_Click(object sender, EventArgs e)
+        {
+            string DATABASE = CONEXION.Database.ToString();
+            if (txtLOCALIZACION1.Text == string.Empty)
+            {
+                MessageBox.Show("Por favor ingrese la ubicación del archivo de copia de seguridad", "ADVERTENCIA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                string cmd = "BACKUP DATABASE [" + DATABASE + "] TO DISK= '" + txtLOCALIZACION1.Text + "\\" + "database" + "-" + DateTime.Now.ToString("yyyy-MM-dd--HH-mm-ss") + ".bak'";
+                CONEXION.Open();
+                SqlCommand COMANDO = new SqlCommand(cmd, CONEXION);
+                COMANDO.ExecuteNonQuery();
+                MessageBox.Show("Copia de seguridad de la base de datos realizada con éxito", "COPIA DE SEGURIDAD", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                CONEXION.Close();
+                btnBACKUP.Enabled = false;
+            }
+        }
 
+        private void btnNAVEGADOR2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "SQL SERVER database backup files |* .bak";
+            dlg.Title = "Restaurar Base de Datos";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtLOCALIZACION2.Text = dlg.FileName;
+                btnRESTAURAR.Enabled = true;
+            }
+        }
+
+        private void btnRESTAURAR_Click(object sender, EventArgs e)
+        {
+            string DATABASE = CONEXION.Database.ToString();
+            CONEXION.Open();
+
+            try
+            {
+                string str1 = string.Format("ALTER DATABASE [" + DATABASE + "] SET SINGLE_USER WITH ROLLBACK IMMEDIATE");
+                SqlCommand cmd1 = new SqlCommand(str1, CONEXION);
+                cmd1.ExecuteNonQuery();
+
+                string str2 = "USE MASTER RESTORE DATABASE [" + DATABASE + "] FROM DISK='" + txtLOCALIZACION2.Text + "' WITH REPLACE;";
+                SqlCommand cmd2 = new SqlCommand(str2, CONEXION);
+                cmd2.ExecuteNonQuery();
+
+                string str3 = string.Format("ALTER DATABASE [" + DATABASE + "] SET MULTI_USER");
+                SqlCommand cmd3 = new SqlCommand(str3, CONEXION);
+                cmd3.ExecuteNonQuery();
+
+                MessageBox.Show("Restauración de la base de datos realizada con éxito", "RESTAURACION BASE DE DATOS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                CONEXION.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error en la restauración de la base de datos: " + ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCERRAR_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
